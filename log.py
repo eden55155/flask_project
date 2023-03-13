7d364ef523a8610aa44de7c7c4782a187772d2d4
eden55155 <100211595+eden55155@users.noreply.github.com>
2023-03-13 20:06:22 +0200

    Create log.py

---
+++ log.py
@@ -0,0 +1,26 @@
+import os
+
+dirs = []
+
+for dir in os.listdir('.'):
+    if dir.isnumeric():
+       dirs.append(dir)
+
+buildNo = sorted(dirs)[-1]
+
+file_obj = open(f"/var/jenkins_home/jobs/Project/builds/{buildNo}/log", "r")
+
+file_data = file_obj.read()
+lines = file_data.splitlines()
+
+for line in lines:
+    if 'Started by' in line:
+     started =line
+    if 'Date:' in line:
+       date = line
+
+finish = lines[-1]
+
+print(started)
+print(date)
+print(finish)
