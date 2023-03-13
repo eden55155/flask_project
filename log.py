fe32446eda5f0cf991b24f84156c729c63c152de
eden55155 <100211595+eden55155@users.noreply.github.com>
2023-03-13 22:08:33 +0200

    Update log.py

--- log.py
+++ log.py
@@ -8,6 +8,7 @@ for dir in os.listdir('/var/jenkins_home/jobs/Project/builds'):
 
 buildNo = sorted(dirs)[-1]
 
+check_file = os.path.exists('successlog.csv')
 file_obj = open(f"/var/jenkins_home/jobs/Project/builds/{buildNo}/log", "r")
 file_data = file_obj.read()
 lines = file_data.splitlines()
@@ -15,12 +16,19 @@ started = ''
 date = ''
 for line in lines:
     if 'Started by' in line:
-     started = line
+        started = line
     if 'Date:' in line:
-       date = line
+        date = line
 
 finish = lines[-1]
 
-print(started)
-print(date)
-print(finish)
+if check_file:
+    file = open("successlog.csv", "a")
+    file.write(started)
+    file.write(date)
+    file.write(finish)
+else:
+    file = open("successlog.csv", "w")
+    file.write(f"{started} \n")
+    file.write(f"{date} \n")
+    file.write(f"{finish} \n")
